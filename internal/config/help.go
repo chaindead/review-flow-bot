0db63ce11aef3e1409084455edb009bb757e81da
John Doe <miromaxxs@gmail.com>
2025-10-12 14:00:27 +0300

    docs: update help

--- internal/config/help.go
+++ internal/config/help.go
@@ -6,8 +6,19 @@ import (
 
 	"github.com/caarlos0/env/v11"
 	"github.com/olekukonko/tablewriter"
+	"github.com/olekukonko/tablewriter/tw"
 	"github.com/rs/zerolog/log"
 )
+
+var descriptionMap = map[string]string{
+	"DB_DB_FILE":          "path to database file",
+	"DB_SEED":             "populate db with fake data for testing purposes",
+	"GITLAB_TOKEN":        "token should have accesses [api, read_api, read_repository]",
+	"GITLAB_URL":          "main url for your gitlab instance",
+	"TG_ADMIN_IDS":        "tg ids of users, who have access to admin commands",
+	"TG_TOKEN":            "tg bot token",
+	"WATCH_POLL_INTERVAL": "period of watching MR status updates",
+}
 
 func Help() {
 	params, err := env.GetFieldParams(&Config{})
@@ -15,8 +26,26 @@ func Help() {
 		log.Fatal().Err(err).Send()
 	}
 
-	table := tablewriter.NewWriter(os.Stdout)
-	table.Header([]string{"Environment Variable", "Default Value", "Current Value"})
+	table := tablewriter.NewTable(
+		os.Stdout,
+		tablewriter.WithConfig(tablewriter.Config{
+			Row: tw.CellConfig{Alignment: tw.CellAlignment{
+				PerColumn: []tw.Align{
+					"",
+					"",
+					"",
+					"",
+					tw.AlignCenter},
+			},
+			},
+		}))
+	table.Header([]string{
+		"Environment Variable",
+		"Description",
+		"Default Value",
+		"Current Value",
+		"Valid",
+	})
 	for _, row := range newTableRows(params) {
 		table.Append(row)
 	}
@@ -39,12 +68,28 @@ func newTableRows(fields []env.FieldParams) [][]string {
 			currentValue = val
 		}
 
-		rows = append(rows, []string{field.Key, defaultValue, currentValue})
+		valid := "✔"
+		if field.Required && currentValue == undefined {
+			valid = "✖✖✖"
+		}
+
+		rows = append(rows, []string{
+			field.Key,
+			descriptionMap[field.Key],
+			defaultValue,
+			currentValue,
+			valid,
+		})
+		delete(descriptionMap, field.Key) // for check unused
 	}
 
 	sort.Slice(rows, func(i, j int) bool {
 		return rows[i][0] < rows[j][0]
 	})
+
+	if len(descriptionMap) != 0 {
+		log.Warn().Interface("unused", descriptionMap).Msg("found unused descriptions")
+	}
 
 	return rows
 }
